026f536ec7ed5b6f6b01cef08590abf3082300a4
Ruan Montelo <ruanmontelo@gmail.com>
2022-01-04 12:48:35 -0300

    Transfering the library to the 'main.go' file, since there is no use for a /funcs route when using the lib

--- main.go
+++ main.go
@@ -3,41 +3,70 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"github.com/JustAn0therDev/LogGo/funcs"
+	"time"
 )
 
-func main() {
-	errorMessage := "testing error"
-	warningMessage := "testing warning"
-	infoMessage := "testing info"
-	funcs.LogError(&errorMessage)
-	funcs.LogWarning(&warningMessage)
-	funcs.LogInfo(&infoMessage)
+const
+(
+	errorLogType string = "ERROR"
+	warningLogType string = "WARNING"
+	infoLogType string = "INFO"
+)
 
-	var response string
-
-	fmt.Print("Do you want to read the log file into the terminal buffer? (y/n) ")
-
-	fmt.Scanln(&response)
-
-	if response != "n" {
-		bytes, err := os.ReadFile("2022-01-03.logGo")
-
-		if err != nil {
-			panic(err)
-		}
-	
-		fmt.Println(convertByteArrayToString(bytes))
-	}
+func getLogCurrentDayFileName() string {
+	year, month, day := time.Now().Date()
+	yearString, monthString, dayString := formatNumberStringWithLeadingZero(year), formatNumberStringWithLeadingZero(int(month)), formatNumberStringWithLeadingZero(day)
+	return fmt.Sprintf("%v-%v-%v.logGo", yearString, monthString, dayString)
 }
 
-func convertByteArrayToString(buf []byte) string {
-	var toReturn []rune
+func createLogFileAndReturnWriter() (*os.File, error) {
+	w, err := os.Create(fmt.Sprintf("%v", getLogCurrentDayFileName()))
 
-	for _, byt := range buf {
-		toReturn = append(toReturn, rune(byt))
+	if err != nil {
+		return nil, err
 	}
 
-	return string(toReturn)
+	return w, nil
+}
+
+func getFormattedTimeString(tm *time.Time) string {
+	hourString := formatNumberStringWithLeadingZero(tm.Hour())
+	minuteString := formatNumberStringWithLeadingZero(tm.Minute())
+	secondString := formatNumberStringWithLeadingZero(tm.Second())
+
+	return fmt.Sprintf("%s:%s:%s", hourString, minuteString, secondString)
+}
+
+func formatNumberStringWithLeadingZero(number int) string {
+	if number < 10 {
+		return fmt.Sprintf("%02d", number)
+	}
+
+	return fmt.Sprint(number)
+}
+
+func LogError(logMessage *string) {
+	writeLogMessage(errorLogType, logMessage)
+}
+
+func LogWarning(logMessage *string) {
+	writeLogMessage(warningLogType, logMessage)
+}
+
+func LogInfo(logMessage *string) {
+	writeLogMessage(infoLogType, logMessage)
+}
+
+func writeLogMessage(logType string, logMessage *string) {
+	fileName := getLogCurrentDayFileName()
+	w, err := os.OpenFile(fileName, os.O_APPEND, os.ModeAppend)
+	if err != nil {
+		w, err = createLogFileAndReturnWriter()
+		if err != nil {
+			fmt.Printf("Program failed to create a new log file! Error: %s\n", err)
+		}
+	}
+
+	currentTime := time.Now()
+	w.WriteString(fmt.Sprintf("[%v] - %v: %v\n", logType, getFormattedTimeString(&currentTime), *logMessage))
 }
